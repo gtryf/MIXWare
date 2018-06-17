c0d0580b1e67dff5f3eaff52d498e47772f01a18
George Tryfonas <george.tryfonas@gmail.com>
2018-06-18 00:08:45 +0300

    Switch to Semantic UI

--- src/MIXUI/ClientApp/src/index.js
+++ src/MIXUI/ClientApp/src/index.js
@@ -1,6 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
+import './semantic/dist/semantic.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
